7561cda424ccdf026a29042a44b154f64e7c4899
Ira Kaidanovich <startel2580startel@gmail.com>
2025-01-16 15:04:19 +0100

    (dark mode)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
     <DarkModeProvider>
-    <Header />
-  </DarkModeProvider>
+      <Header />
+    </DarkModeProvider>
     <router-view />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import DarkModeProvider from "./components/DarkModeProvider.vue"
+import DarkModeProvider from "./components/DarkModeProvider.vue";
 
 export default {
-  name: "App", 
+  name: "App",
   components: {
-    Header, 
+    Header,
   },
 };
 </script>
@@ -32,20 +32,36 @@ export default {
   -moz-osx-font-smoothing: grayscale;
 }
 
-@media (max-width:800px) {
+:root {
+  --background-color: #ffffff;
+  --text-color: #000000;
+}
+
+body.dark-mode {
+  --background-color: #121212;
+  --text-color: #ffffff;
+}
+
+body {
+  background-color: var(--background-color);
+  color: var(--text-color);
+  transition: background-color 0.3s ease, color 0.3s ease;
+}
+
+@media (max-width: 800px) {
   #app {
     display: flex;
     flex-direction: column-reverse;
   }
 }
 
-html, body {
+html,
+body {
   margin: 0;
   padding: 0;
   overflow-x: hidden;
   width: 100%;
 }
-
 
 body {
   margin: 0px;
